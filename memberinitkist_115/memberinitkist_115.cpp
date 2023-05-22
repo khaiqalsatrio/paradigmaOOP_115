31ebfbe79f6990aeb07287283220f2e3335ddcf5
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-05-22 19:16:08 +0700

    class mahasiswa

--- memberinitkist_115/memberinitkist_115.cpp
+++ memberinitkist_115/memberinitkist_115.cpp
@@ -8,20 +8,3 @@ private:
 	string nama;
 	float nilai;
 
-public:
-	mahasiswa(int pid, string pNama, float pNilai) :id(pid), nama(pNama), nilai(pNilai) { //member initialization list
-
-	}
-
-	~mahasiswa() {
-		cout << "id     = " << id << endl;
-		cout << "id     = " << nama << endl;
-		cout << "Nilai  = " << nilai << endl;
-	}
-};
-
-int main() {
-	mahasiswa mhs(12, "Asroni", 90.5);
-
-	return 0;
-}
