a57026236f87647188d79416ed5f20a7856fcf51
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-05-22 19:22:05 +0700

    ibu.h

--- mainlA_115/ibu.h
+++ mainlA_115/ibu.h
@@ -6,3 +6,23 @@ public:
 	string nama;
 	vector<anak*> daftar_anak;
 
+	ibu(string pNama) :nama(pNama) {
+		cout << "ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "ibu \"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
+};
+void ibu::tambahAnak(anak* pAnak) {
+	daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak() {
+	cout << "daftar Anak dari ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+#endif
